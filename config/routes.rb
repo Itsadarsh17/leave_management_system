e4f7a0efb0a331deee9d23df886fb5c292825d3d
itsadarsh17 <adarsh9773276076@gmail.com>
2024-05-28 10:49:21 +0530

    csv and mailer changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,36 @@
 Rails.application.routes.draw do
-  root 'leave_applications#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
 
-  resources :users
-  resources :roles
-  resources :leave_applications
+  resources :users, only: [:index,:show] do
+    member do
+      get 'leave_details'
+      post :accrue_leaves
+    end
+  end
 
+  resources :leave_applications do
+    member do
+      put :accept
+      put :reject
+    end
+    collection do
+      get :export_csv
+    end
+  end
+
+  authenticated :user, ->(u) { u.admin? } do
+    get '/admin_dashboard', to: 'admin_dashboard#index'
+    get '/leave_request', to: 'admin_dashboard#leave_request'
+  end
+
+  authenticated :user do
+    get '/user_dashboard', to: 'dashboard#index' , as: 'user_dashboard'
+  end
+
+  unauthenticated do
+    root 'leave_applications#index'
+  end
 end
